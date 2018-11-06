b7e1ea4fc0bb2b793514fd24a80e4253d8d83117
z <phxv@mail.ru>
2018-11-06 08:47:20 +0300

    1

--- nn_utils.py
+++ nn_utils.py
@@ -7,12 +7,12 @@ def copyModelToModel(source_model, target_model, certain_layer=""):
 	print("model source was copied into model target")
 
 
-def copy_model_weights(model_source, model_target, start_layer=249):
-	num_layers = len(model.layers)
+def copy_model_weights(source_model, target_model, start_layer=249):
+	num_layers = len(target_model.layers)
 	print(len(source_model.layers), 'layers in source model')
 	print(num_layers, 'layers in target model')
 	for i in range(start_layer, num_layers):
 		weights = source_model.layers[i].get_weights()
-		model.layers[i].set_weights(weights)
-		model.layers[i].trainable = True
+		target_model.layers[i].set_weights(weights)
+		target_model.layers[i].trainable = True
 
