ececdd24e60c315ab249a528c87e49f436f1c109
z <phxv@mail.ru>
2018-11-06 14:58:03 +0300

    1

--- settings.py
+++ settings.py
@@ -3,10 +3,11 @@ num_classes = 148
 IMAGE_SIZE = (299, 299)
 multiply = 5
 valid_percentage = 0.1
+train_batch = 32
+valid_batch = 32
 #train_batch = 32
 #valid_batch = 32
-train_batch = 10
-valid_batch = 10
+
 # 'sort' or 'shuffle'
 dataset_order = 'shuffle' 
 # a1 and b0 coeff. (max values), see https://www.tensorflow.org/api_docs/python/tf/contrib/image/transform
