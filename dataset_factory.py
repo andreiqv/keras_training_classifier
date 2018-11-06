68e06971691988e0ef2d8a7fc8f46dc19f485fdc
z <phxv@mail.ru>
2018-11-06 10:48:20 +0300

    1

--- dataset_factory.py
+++ dataset_factory.py
@@ -222,7 +222,9 @@ class GoodsDataset:
             #print('Added transformations:', transform)
             images = tf.contrib.image.transform(images, transform1)            
             # ---
-            images = tf.random_crop(images, [batch, 100, 100, 3])
+            w_crop = math.ceil(IMAGE_SIZE[0] / 1.2)
+            h_crop = math.ceil(IMAGE_SIZE[1] / 1.2)
+            images = tf.random_crop(images, [batch, h_crop, w_crop, 3])
             # ---
             images = tf.image.resize_image_with_crop_or_pad(images, w, h)
             # end of Rotation and Transformation block
