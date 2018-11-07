e9a299d69e51dfdef3e2b357c248447539938b41
z <phxv@mail.ru>
2018-11-07 16:16:56 +0300

    1

--- settings.py
+++ settings.py
@@ -1,5 +1,4 @@
 # dataset parameters
-num_classes = 148
 IMAGE_SIZE = (299, 299)
 multiply = 5
 valid_percentage = 0.1
@@ -13,3 +12,11 @@ dataset_order = 'shuffle'
 # a1 and b0 coeff. (max values), see https://www.tensorflow.org/api_docs/python/tf/contrib/image/transform
 transform_maxval = 0.2 
 rotation_max_angle = 90
+
+dataset_list = 'dataset-181018.list'
+labels_list = 'dataset-181018.labels'
+
+#num_classes = 148
+f = open(labels_list)
+['1' for x in f.readlines() if len(x.strip()) > 0]
+print('num_classes:', num_classes)
