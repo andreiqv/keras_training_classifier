76fda39528e71414af6dd407e19b29376707fc80
z <phxv@mail.ru>
2018-11-07 17:56:22 +0300

    1

---
+++ tflearn/convnet_own_dataset.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+
+""" Convolutional network applied to CIFAR-10 dataset classification task.
+
+References:
+    Learning Multiple Layers of Features from Tiny Images, A. Krizhevsky, 2009.
+
+Links:
+    [CIFAR-10 Dataset](https://www.cs.toronto.edu/~kriz/cifar.html)
+
+"""
+from __future__ import division, print_function, absolute_import
+
+import tflearn
+from tflearn.data_utils import shuffle, to_categorical
+from tflearn.layers.core import input_data, dropout, fully_connected
+from tflearn.layers.conv import conv_2d, max_pool_2d
+from tflearn.layers.estimator import regression
+from tflearn.data_preprocessing import ImagePreprocessing
+from tflearn.data_augmentation import ImageAugmentation
+
+# Data loading and preprocessing
+from tflearn.data_utils import image_preloader
+
+#from tflearn.datasets import cifar10
+train_dataset_file = 'dataset-181018.list' 
+val_dataset_file = 'dataset-181018.list'
+
+(X, Y), (X_test, Y_test) = cifar10.load_data()
+X, Y = image_preloader(train_dataset_file, image_shape=(32, 32),       #use the 'image_preloader'                    
+	mode='file', categorical_labels=True, normalize=True)
+X_val, Y_val = image_preloader(val_dataset_file, image_shape=(32, 32),
+	mode='file', categorical_labels=True, normalize=True)
+
+nb_classes = 148
+X, Y = shuffle(X, Y)
+Y = to_categorical(Y, nb_classes)
+Y_test = to_categorical(Y_test, nb_classes)
+
+# Real-time data preprocessing
+img_prep = ImagePreprocessing()
+img_prep.add_featurewise_zero_center()
+img_prep.add_featurewise_stdnorm()
+
+# Real-time data augmentation
+img_aug = ImageAugmentation()
+img_aug.add_random_flip_leftright()
+img_aug.add_random_rotation(max_angle=25.)
+
+# Convolutional network building
+network = input_data(shape=[None, 32, 32, 3],
+                     data_preprocessing=img_prep,
+                     data_augmentation=img_aug)
+network = conv_2d(network, 32, 3, activation='relu')
+network = max_pool_2d(network, 2)
+network = conv_2d(network, 64, 3, activation='relu')
+network = conv_2d(network, 64, 3, activation='relu')
+network = max_pool_2d(network, 2)
+network = fully_connected(network, 512, activation='relu')
+network = dropout(network, 0.5)
+network = fully_connected(network, 10, activation='softmax')
+network = regression(network, optimizer='adam',
+                     loss='categorical_crossentropy',
+                     learning_rate=0.001)
+
+# Train using classifier
+model = tflearn.DNN(network, tensorboard_verbose=0)
+model.fit(X, Y, n_epoch=50, shuffle=True, validation_set=(X_test, Y_test),
+          show_metric=True, batch_size=96, run_id='cifar10_cnn')
