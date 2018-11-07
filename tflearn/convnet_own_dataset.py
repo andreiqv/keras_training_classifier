0ea1928ea9c91ea9c9b768968f7be4edba530d41
z <phxv@mail.ru>
2018-11-07 18:06:18 +0300

    1

--- tflearn/convnet_own_dataset.py
+++ tflearn/convnet_own_dataset.py
@@ -34,6 +34,7 @@ X, Y = image_preloader(train_dataset_path, mode='folder', image_shape=(32, 32), 
 X_val, Y_val = image_preloader(val_dataset_path, mode='folder', image_shape=(32, 32),
 	categorical_labels=True, normalize=True)
 X_test, Y_test = X_val, Y_val
+print('image_preloader done')
 
 nb_classes = 148
 X, Y = shuffle(X, Y)
@@ -50,6 +51,8 @@ img_aug = ImageAugmentation()
 img_aug.add_random_flip_leftright()
 img_aug.add_random_rotation(max_angle=25.)
 
+
+print('Convolutional network building')
 # Convolutional network building
 network = input_data(shape=[None, 32, 32, 3],
                      data_preprocessing=img_prep,
