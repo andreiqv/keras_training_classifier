4ea07b04059e3d15cc92b45b670f812c717d3cec
z <phxv@mail.ru>
2018-11-09 16:18:24 +0300

    1

--- fine_tune_model_4_direct_copy_top60weights.py
+++ fine_tune_model_4_direct_copy_top60weights.py
@@ -148,7 +148,7 @@ valid_dataset = goods_dataset.get_valid_dataset()
 results = model.evaluate(goods_dataset.get_images_for_label(94).batch(16).repeat(), steps=6)
 print(results)
 
-model.fit(train_dataset.prefetch(5).repeat(), # was prefetch(2)
+model.fit(train_dataset.prefetch(16).repeat(), # was prefetch(2)
           callbacks=callbacks,
           epochs=30,
           steps_per_epoch=1157,
@@ -222,8 +222,11 @@ Epoch 3/30 - loss: 1.0501 - acc: 0.6793 - top_6: 0.9448 - val_loss: 1.1757 - val
 Epoch 6/30 - loss: 0.9210 - acc: 0.7164 - top_6: 0.9569 - val_loss: 1.1942 - val_acc: 0.6593 - val_top_6: 0.9294
 Epoch 10/30- loss: 0.8422 - acc: 0.7362 - top_6: 0.9657 - val_loss: 1.1846 - val_acc: 0.6710 - val_top_6: 0.9290
 
-тоже самое с multi-gpu (gpus=3) - 721s 623ms/step
+тоже самое с multi-gpu (gpus=2) - 721s 623ms/step
 Epoch 1/30 - loss: 1.5800 - acc: 0.5678 - top_6: 0.8869 - val_loss: 1.1963 - val_acc: 0.6538 - val_top_6: 0.9282
+
+с multi-gpu (gpus=4)
+
 
 
 """
