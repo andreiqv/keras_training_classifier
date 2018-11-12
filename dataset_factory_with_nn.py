2a1785f25f7e51df53e596c9431017f39b737c6b
z <phxv@mail.ru>
2018-11-12 11:13:52 +0300

    1

--- dataset_factory_with_nn.py
+++ dataset_factory_with_nn.py
@@ -123,7 +123,7 @@ class GoodsDataset:
 
 			inputs = tf.placeholder(tf.float32, [None, IMAGE_SIZE[0], IMAGE_SIZE[1], 3])
 
-			model_number = 1
+			model_number = 2
 			# Use keras InceptionV3 model: 
 			
 			if model_number == 1:
